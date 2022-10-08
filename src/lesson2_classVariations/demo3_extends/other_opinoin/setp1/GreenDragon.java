270ee98306358f9195cfa55cc4faf2481f330aae
GrayHead <ss.zhuravlov@gmail.com>
2022-10-08 14:52:30 +0300

    інший погля д на наслідування

---
+++ src/lesson2_classVariations/demo3_extends/other_opinoin/setp1/GreenDragon.java
@@ -0,0 +1,10 @@
+package lesson2_classVariations.demo3_extends.other_opinoin.setp1;
+
+import lesson2_classVariations.demo3_extends.other_opinoin.Type;
+
+public class GreenDragon {
+    public int dmg;
+    public int def;
+    public Type type = Type.FLY;
+    public String buff;
+}
